4fe063e9ac3a67ecaacceff262ffdf1cf4ddef5f
CallMeMatheist <mathisgagnon@gmail.com>
2023-02-17 22:29:27 -0500

    good

--- Assets/asd.cs
+++ Assets/asd.cs
@@ -7,7 +7,7 @@ public class asd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       penis 
+      
     }
 
     // Update is called once per frame
