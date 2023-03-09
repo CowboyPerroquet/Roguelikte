c82ba4a849556dbb3f8271aa4aa97c81a14c0829
CowboyPerroquet <simonolivier.dore@gmail.com>
2023-03-09 08:24:05 -0500

    DebutMenuUI
    MenuUI Noncomplete

--- Assets/DimensionScript.cs
+++ Assets/DimensionScript.cs
@@ -1,40 +1,63 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 public class DimensionScript : MonoBehaviour
 {
-    private InputField largeurMur;
-    private InputField longeurMur;
-    private InputField hauteurMurArriere;
-    private InputField hauteurMurAvant;
-    private InputField hauteurMurDroit;
-    private InputField hauteurMurGauche;
+    private TextMeshProUGUI largeurMur;
+    private TextMeshProUGUI longeurMur;
+    private TextMeshProUGUI hauteurMurArriere;
+    private TextMeshProUGUI hauteurMurAvant;
+    private TextMeshProUGUI hauteurMurDroit;
+    private TextMeshProUGUI hauteurMurGauche;
     //will be receive by the name of the button we click
     public GameObject prefabOfSalle;
     public GameObject PanelForDimension;
+    public int boutonId;
+    static private Dictionary<int, GameObject> matchboutton = new Dictionary<int, GameObject>();
+
+    static private int id = 0;
+    void Start()
+    {
+        setDimension();
+
+    }
+
     public void onClickForDimension()
     {
-        
+
         PanelForDimension.SetActive(true);
-        setDimension();
+        SeeDimension();
+       
+
+    }
+
+    private void SeeDimension()
+    {
+
+
+    }
+
+    private void modifyDimesion()
+    {
 
     }
 
     private void setDimension()
     {
+        id = id + 1;
+        Debug.Log(prefabOfSalle.name);
         var prefab = Instantiate(prefabOfSalle);
-       // PanelForDimension= prefab.transform.localScale.z.ToString();
-        longeurMur.text = prefab.transform.localScale.x.ToString();
-        hauteurMurArriere.text = prefab.transform.Find("MurArriere").gameObject.transform.localScale.y.ToString();
-        hauteurMurAvant.text = prefab.transform.Find("MurAvant").gameObject.transform.localScale.y.ToString();
-        hauteurMurDroit.text = prefab.transform.Find("MurDroit").gameObject.transform.localScale.y.ToString();
-        hauteurMurGauche.text = prefab.transform.Find("MurGauche").gameObject.transform.localScale.y.ToString();
+        prefab.name = prefab.name + id;
+        Debug.Log(prefab.name.ToString());
+        PanelForDimension.transform.Find("DonneeLongueur").Find("Text Area").gameObject.GetComponentInChildren<TextMeshProUGUI>().text = prefab.transform.localScale.x.ToString(); //prefab.transform.localScale.z.ToString();
+        PanelForDimension.transform.Find("DonneeLargeur").Find("Text Area").gameObject.GetComponentInChildren<TextMeshProUGUI>().text = prefab.transform.localScale.z.ToString();
+        PanelForDimension.transform.Find("DonneeHauteurArriere").Find("Text Area").gameObject.GetComponentInChildren<TextMeshProUGUI>().text = prefab.transform.Find("MurArriere").gameObject.transform.localScale.y.ToString();
+        PanelForDimension.transform.Find("DonneeHauteurAvant").Find("Text Area").gameObject.GetComponentInChildren<TextMeshProUGUI>().text = prefab.transform.Find("MurAvant").gameObject.transform.localScale.y.ToString();
+        PanelForDimension.transform.Find("DonneeHauteurDroit").Find("Text Area").gameObject.GetComponentInChildren<TextMeshProUGUI>().text = prefab.transform.Find("MurDroit").gameObject.transform.localScale.y.ToString();
+        PanelForDimension.transform.Find("DonneeHauteurGauche").Find("Text Area").gameObject.GetComponentInChildren<TextMeshProUGUI>().text = prefab.transform.Find("MurGauche").gameObject.transform.localScale.y.ToString();
 
     }
 
 }
-    // Start is called before the first frame update
- 
+// Start is called before the first frame update
+
